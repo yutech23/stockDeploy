867c25f30abe6704a46c3595920eac6fc3b1237a
Yusuf C <yusufcoskun23@gmail.com>
2022-11-30 20:42:05 +0300

    yeni site security storega sınıf oluşturuldu

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -278,9 +278,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "561f09114499cbb7b69c11e4efc5fcf2",
-"/": "561f09114499cbb7b69c11e4efc5fcf2",
-"main.dart.js": "9482c605da7140c344f31c6be24f8ee1",
+"index.html": "4b3616c9b7464b4d4edd33260da2cd1e",
+"/": "4b3616c9b7464b4d4edd33260da2cd1e",
+"main.dart.js": "c946fdf3e610a2165a48faf4f234f937",
 "manifest.json": "03871feb49187022544858572311ce39",
 "version.json": "b0f930d7567418c836212908e037bb24"
 };

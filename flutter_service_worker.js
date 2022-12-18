e23ccd17a7592b799ccaa410b6b397740ae752af
Yusuf C <yusufcoskun23@gmail.com>
2022-12-18 19:40:50 +0300

    login sayfaya ayarlandı

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -296,9 +296,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "dcf34b45c6c580076905867c7ee73326",
-"/": "dcf34b45c6c580076905867c7ee73326",
-"main.dart.js": "6727d098073652c5a16a12428b101cb9",
+"index.html": "b93d7e1220782f3757deacdf9fbcc815",
+"/": "b93d7e1220782f3757deacdf9fbcc815",
+"main.dart.js": "84b32bbc3755316e31221aac80cafd76",
 "manifest.json": "03871feb49187022544858572311ce39",
 "version.json": "b0f930d7567418c836212908e037bb24"
 };

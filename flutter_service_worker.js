8969637a6badd632e02e2055f64e4cff6c23b735
Yusuf C <yusufcoskun23@gmail.com>
2022-12-21 13:12:19 +0300

    a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -296,8 +296,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "130c20ee77feec16b56d0b0a5cd1df90",
-"/": "130c20ee77feec16b56d0b0a5cd1df90",
+"index.html": "13b1dff706b1db667513d2fe6d866889",
+"/": "13b1dff706b1db667513d2fe6d866889",
 "main.dart.js": "a8c58b9a087b31735e3b5da2d5217764",
 "manifest.json": "03871feb49187022544858572311ce39",
 "version.json": "b0f930d7567418c836212908e037bb24"

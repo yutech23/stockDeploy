6d8a28a7817ca0b3ebafc94b09e1ec762b27f673
Yusuf C <yusufcoskun23@gmail.com>
2023-01-05 16:29:27 +0300

    login ileri ikonu eklendi

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -296,9 +296,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6c8ef485e80fb81cb06af07a7e600708",
-"/": "6c8ef485e80fb81cb06af07a7e600708",
-"main.dart.js": "7d495f49492f2c39fd1606a0dd25b5b2",
+"index.html": "d81660970d17eea972fb7a582990ce2e",
+"/": "d81660970d17eea972fb7a582990ce2e",
+"main.dart.js": "7b9875e962ca74ba6f30d2878b49614c",
 "manifest.json": "03871feb49187022544858572311ce39",
 "version.json": "b0f930d7567418c836212908e037bb24"
 };

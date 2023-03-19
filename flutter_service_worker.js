e3e07a2c58a04a95c7e22c4ff6fbf1a8436dc4b6
Yusuf C <yusufcoskun23@gmail.com>
2023-03-19 21:36:43 +0300

    sale sayfa ürün ekleme düzeltildi

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -297,9 +297,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5cd8c83b9234e91c750e51004839a0a1",
-"/": "5cd8c83b9234e91c750e51004839a0a1",
-"main.dart.js": "3f8d20329188a25dcc16d614516c1be4",
+"index.html": "9252ce2ba7441f52206d01191dd3dd70",
+"/": "9252ce2ba7441f52206d01191dd3dd70",
+"main.dart.js": "0dcb289e52a7decb026d5079f9ca7bf7",
 "manifest.json": "03871feb49187022544858572311ce39",
 "version.json": "b0f930d7567418c836212908e037bb24"
 };

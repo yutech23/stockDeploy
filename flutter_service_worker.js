3662980419499f47c5df4b55c8282a7a96d7edc7
Yusuf C <yusufcoskun23@gmail.com>
2023-03-22 03:00:53 +0300

    son düzenleme ve güncelleme

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -298,8 +298,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8770ee03cde2dd7845a245a69c5d9b0b",
-"/": "8770ee03cde2dd7845a245a69c5d9b0b",
+"index.html": "6a1eaeb47339da3dbe2b1b37fc400269",
+"/": "6a1eaeb47339da3dbe2b1b37fc400269",
 "main.dart.js": "fdf0bedaac0630f9bc8d80e0d676e7c3",
 "manifest.json": "03871feb49187022544858572311ce39",
 "version.json": "b0f930d7567418c836212908e037bb24"
